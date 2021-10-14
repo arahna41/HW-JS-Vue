dcac71a4f167da31f4d496f180d1978d6da76236
arahna41 <arahna41@gmail.com>
2021-10-14 08:41:28 +0300

    add task 2.2

--- HW_2.js
+++ HW_2.js
@@ -1,11 +1,27 @@
-var user = {name: 'Nata'}
+//task HW 2.1
 
-user.sayHello = function(greetings = 'Добрый день') {
-	if (this.name != undefined && typeof(this.name) == 'string' && this.name != '') {
-		return (`${greetings}, ${this.name}!`)
-	} else {
-		return ('Имя пользователя не указано')
-	}
+var user = { name: "Nata" }
+
+user.sayHello = function (greetings = "Добрый день") {
+  if (
+    this.name != undefined &&
+    typeof this.name == "string" &&
+    this.name != ""
+  ) {
+    return `${greetings}, ${this.name}!`
+  } else {
+    return "Имя пользователя не указано"
+  }
 }
 
+//task HW 2.2
 
+var product = {
+  name: "iPhone",
+  id: 7485,
+  price: 1000
+}
+
+product.changePrice = function(num) {
+  return (this.price += (this.price * (num / 100)))
+}
