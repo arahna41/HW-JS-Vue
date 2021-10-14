d27755c36790b22aae253590bf20cfc995a0e4d3
arahna41 <arahna41@gmail.com>
2021-10-14 09:57:42 +0300

    add task 2.3

--- HW_2.js
+++ HW_2.js
@@ -23,5 +23,32 @@ var product = {
 }
 
 product.changePrice = function(num) {
-  return (this.price += (this.price * (num / 100)))
+  return ( this.price += (this.price * (num / 100) ) )
+}
+
+// task HW 2.3
+
+function showStudentsFavoriteLessons() {
+  var studentsList = showStudentsFavoriteLessons.arguments
+
+  var lessons = ['Математика', 'История', 'Спорт', 'Литература', 'Биология', 'География']
+  
+  function checkStudentFavoriteLessons() {
+	  for (var i = 0; i < studentsList.length; i++) {
+      var favoriteLessons = []
+      favoriteLessons = lessons.slice( getRandomInt(0, 6), 6 )
+      function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor( Math.random() * (max - min + 1) ) + min;
+      }
+      if (favoriteLessons.length != 0) {
+        console.log(`${studentsList[i]}: ${favoriteLessons.join(', ')}`)
+      } else {
+        console.log(`${studentsList[i]}: У этого студента нет любимых предметов`)
+      }
+    }
+  }
+
+  checkStudentFavoriteLessons(studentsList)
 }
