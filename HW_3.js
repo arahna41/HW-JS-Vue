749d279859f471ad360f7afcae69787828fa7d48
arahna41 <arahna41@gmail.com>
2021-10-18 15:50:48 +0300

    task 3.3

--- HW_3.js
+++ HW_3.js
@@ -2,7 +2,7 @@
 
 
 
-//task3.2
+//task 3.2
 
 function fixedArray(numArray, stringArray) {
   if (numArray.length == stringArray.length) {
@@ -16,5 +16,17 @@ function fixedArray(numArray, stringArray) {
     return (newArray.slice(0, (newArray.length / 2) ))
   } else {
     return ('Массивы имеют разную длину')
+  }
+}
+
+//task 3.3
+
+function checkLengthString(string, num) {
+  if (string.length > +num) {
+    return (`${string}...`)
+  } else if (string.length < +num) {
+    var result = string[0].toUpperCase() + string.substring(1).split(' ').join('-').slice(string[string.length - 1]).substring(0, string.length - 2);
+    var lastLetter = string[string.length - 1].toUpperCase()
+    return (result + lastLetter)
   }
 }
